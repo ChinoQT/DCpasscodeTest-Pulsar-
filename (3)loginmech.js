798261ee1c3bf0efd6e3671714b2c238f3120d23
ChiiinoQT <86642399+ChinoQT@users.noreply.github.com>
2021-11-22 19:23:27 +0800

    Add files via upload

---
+++ (3)loginmech.js
@@ -0,0 +1,13 @@
+
+
+function testfunc() {
+    var inputt = document.getElementById("Pswinput").value;
+
+    if (inputt == "Pulsar" || inputt == "pulsar") {
+        document.getElementById("pindot").href="https://discord.gg/kVxZ6YQYUY";
+    }
+    else {
+        alert("Denied")
+    }
+    
+}
